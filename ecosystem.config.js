b81d823247c6d4c18ab262f0e97fb3d11bef8a24
dharmikBhadra83 <dharmikbhadra8@gmail.com>
2026-01-21 17:58:16 +0530

    added deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [{
+    name: 'monitra',
+    script: 'node_modules/next/dist/bin/next',
+    args: 'start',
+    cwd: '/home/ubuntu/monitra',
+    instances: 1,
+    exec_mode: 'fork',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    },
+    error_file: '/var/log/pm2/monitra-error.log',
+    out_file: '/var/log/pm2/monitra-out.log',
+    log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+    merge_logs: true,
+    autorestart: true,
+    max_memory_restart: '1G',
+    watch: false,
+    ignore_watch: ['node_modules', '.next', '.git']
+  }]
+};
